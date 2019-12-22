e6ff8874798822bc4c37359be8f2cf33db829e03
damienmontastier <montastier.damien@gmail.com>
2019-12-22 03:50:54 +0100

    wip init

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,14 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        herve-portfolio
-      </h1>
-      <h2 class="subtitle">
-        My divine Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+      <h1 class="title">herve-portfolio</h1>
+      <h2 class="subtitle">My divine Nuxt.js project</h2>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
 <style>
@@ -37,8 +22,7 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Gandur', 'Source Sans Pro';
   display: block;
   font-weight: 300;
   font-size: 100px;
