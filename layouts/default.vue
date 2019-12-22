8edfc2f03b666b15936ea8688e11d5ee018a1f5a
damienmontastier <montastier.damien@gmail.com>
2019-12-22 21:49:00 +0100

    wip netlify cms

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,13 @@ import Nav from '@/components/Nav'
 import Pointer from '@/components/Cursor'
 
 export default {
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      ]
+    }
+  },
   components: {
     Nav,
     Pointer
--- store/index.js
+++ store/index.js
@@ -1,9 +1,26 @@
 export const state = () => ({
-  counter: 0
+  blogPosts: []
 })
 
 export const mutations = {
-  // increment (state) {
-  //   state.counter++
-  // }
+  setBlogPosts(state, list) {
+    state.blogPosts = list
+  }
+}
+
+export const actions = {
+  async nuxtServerInit({ commit }) {
+    let files = await require.context(
+      '~/assets/content/blog/',
+      false,
+      /\.json$/
+    )
+    console.log(files)
+    // let blogPosts = files.keys().map((key) => {
+    //   let res = files(key)
+    //   res.slug = key.slice(2, -5)
+    //   return res
+    // })
+    // await commit('setBlogPosts', blogPosts)
+  }
 }
